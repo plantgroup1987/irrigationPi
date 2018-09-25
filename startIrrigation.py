dc8263327613e028eb84853b5dcea6acfc182cab
Austin Arrington <austinarrington@Austins-MacBook-Pro.local>
2018-09-24 23:08:01 -0400

    updated startIrrigation

--- startIrrigation.py
+++ startIrrigation.py
@@ -2,7 +2,8 @@
 import time
 import serial, io, sys
 import RPi.GPIO as GPIO
-import urllib
+import urllib.request, json
+import time, datetime
 #pip3 install wifi // pip3 install wireless 
 from wifi import Cell, Scheme
 from wireless import Wireless
@@ -101,3 +102,46 @@ else:
     GPIO.setup(ledIndic, GPIO.OUT)
     GPIO.output(ledIndic, GPIO.LOW)
     print("Succesfully connected to WiFi network.")
+    
+# API Calls
+# this is where request que was in init.lua
+url = "http://app.plantgroup.co/api/controllers/"+str(deviceID)+"/config/"
+response = urllib.request.urlopen(url).read()
+resp2 = str(response)[2:len(str(response))-1]
+jsonForm = json.loads(resp2)
+# parse jsonForm
+thingspeakUpdate=((jsonForm["config"]["system"]["sync_interval_seconds"])*1000)
+systemClock=(jsonForm["config"]["system"]["system_clock_seconds"])*1000
+soilThres=(jsonForm["config"]["user"]["soil_moisture_threshold"])
+waterDelay=(jsonForm["config"]["user"]["watering_delay_seconds"])*1000
+waterDuration=(jsonForm["config"]["user"]["watering_duration_seconds"])*1000
+intervals = len(jsonForm["config"]["user"]["watering_schedule"])
+serverTime = jsonForm["local_time"]
+# print parsed form results 
+print("ThingSpeak Update: "+str(thingspeakUpdate))
+print("System Clock: "+str(systemClock))
+print("Soil Moisture Threshold: " +str(soilThres))
+print("Water Delay: " +str(waterDelay))
+print("Water Duration: "+str(waterDuration))
+print("Intervals: "+str(intervals))
+print("Server Time: "+str(serverTime))
+#function to convert times into seconds
+def secConv(x):
+	secs = time.strptime(x,'%H:%M:%S')
+	return datetime.timedelta(hours=secs.tm_hour,
+		minutes=secs.tm_min,
+		seconds=secs.tm_sec).total_seconds()
+# interval building
+stop={}
+begin={}
+for i in range(0,intervals):
+	stop[i]=secConv((jsonForm["config"]["user"]["watering_schedule"][i]["end"])+":00")
+	begin[i]=secConv((jsonForm["config"]["user"]["watering_schedule"][i]["begin"])+":00")
+
+for i in range(0,intervals):
+	print("Stop: "+str(stop[i]))
+	print("Begin: "+str(begin[i]))
+
+print("Water Delay: "+str(waterDelay))
+
+
